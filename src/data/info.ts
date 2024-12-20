fd12c2535e7d11938df82acce2f7ee947250a283
Jairo Morales Pérez <82318955+Icky17@users.noreply.github.com>
2024-12-20 22:15:10 +0100

    Initialize add-license-1

---
+++ src/data/info.ts
@@ -0,0 +1,103 @@
+export const info = {
+  baseUrl: "https://astro-portfolio-uzair.vercel.app",
+  name: "Uzair Ahmed",
+  jobDescription: "Web Developer",
+  about: `I'm a software engineer based in Toronto, Canada and also a
+  communication and journalism student. I enjoy creating things that live
+  on the internet, whether that be websites, applications, or anything in
+  between. I have been freelancing for a year now while studying at the
+  university and I've manage to gain a decent amount of experience and
+  valuable knowledge from all different kinds of fields throughout my
+  projects/work.`,
+
+  experience: [
+    {
+      name: "Microsoft Corporation",
+      location: " Selfbook / US - New York",
+      startDate: "Jul 2021",
+      endDate: "Aug 2021",
+      description: [
+        "- Developing screens and UI components for the web application using React and Tailwind.",
+        "- Fixing UI issues and integrating backend APIs with Redux Saga.",
+      ],
+    },
+    {
+      name: "Microsoft Corporation",
+      location: " Selfbook / US - New York",
+      startDate: "Jul 2021",
+      endDate: "Aug 2021",
+      description: [
+        "- Developing screens and UI components for the web application using React and Tailwind.",
+        "- Fixing UI issues and integrating backend APIs with Redux Saga.",
+      ],
+    },
+    {
+      name: "Microsoft Corporation",
+      location: " Selfbook / US - New York",
+      startDate: "Jul 2021",
+      endDate: "Aug 2021",
+      description: [
+        "- Developing screens and UI components for the web application using React and Tailwind.",
+        "- Fixing UI issues and integrating backend APIs with Redux Saga.",
+      ],
+    },
+    {
+      name: "Microsoft Corporation",
+      location: " Selfbook / US - New York",
+      startDate: "Jul 2021",
+      endDate: "Aug 2021",
+      description: [
+        "- Developing screens and UI components for the web application using React and Tailwind.",
+        "- Fixing UI issues and integrating backend APIs with Redux Saga.",
+      ],
+    },
+    {
+      name: "Microsoft Corporation",
+      location: " Selfbook / US - New York",
+      startDate: "Jul 2021",
+      endDate: "Aug 2021",
+      description: [
+        "- Developing screens and UI components for the web application using React and Tailwind.",
+        "- Fixing UI issues and integrating backend APIs with Redux Saga.",
+      ],
+    },
+  ],
+
+  education: [
+    {
+      name: "University of Toronto",
+      location: "Canada",
+      startDate: "2018",
+      endDate: "2021",
+      description: ["- Bachelor's degree in communication"],
+    },
+    {
+      name: "University of Toronto",
+      location: "Canada",
+      startDate: "2018",
+      endDate: "2021",
+      description: ["- Bachelor's degree in communication"],
+    },
+    {
+      name: "University of Toronto",
+      location: "Canada",
+      startDate: "2018",
+      endDate: "2021",
+      description: ["- Bachelor's degree in communication"],
+    },
+    {
+      name: "University of Toronto",
+      location: "Canada",
+      startDate: "2018",
+      endDate: "2021",
+      description: ["- Bachelor's degree in communication"],
+    },
+  ],
+  socialMedia: {
+    facebook: "https://www.facebook.com/profile.php?id=100022899849583",
+    twitter: "https://twitter.com/Uzzii_21",
+    github: "https://github.com/uzzii-21",
+    email: "uzairahmed@74372gmail.com",
+    linkedin: "https://www.linkedin.com/in/uzzii21/",
+  },
+};
